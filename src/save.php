e6962506ac0aebc4ca3d853b90878fe39f44b708
Matias Lenci <matiaslenci@gmail.com>
2023-08-23 22:49:51 -0300

    query save

--- src/save.php
+++ src/save.php
@@ -1,5 +1,6 @@
 <?php
 
+# traemos la conexion a la base de datos
 include('db.php');
 
 # Pregunto si recibo datos por POST y save_task no es nulo
@@ -8,7 +9,19 @@ if (isset($_POST['save_task'])) {
   $title = $_POST['title'];
   $description = $_POST['description'];
  
-  
+  # Creo la consulta para guardar los datos 
+  ## Insertamos en la tabla task los datos que recibimos
+  $query = "INSERT INTO task(title, description) VALUES ('$title', '$description')";
+
+  # Ejecuto la consulta 
+  ## Pasamps la conexion y la consulta
+  $result = mysqli_query($conn, $query);
+
+  if (!$result) {
+    die('Query Failed.');
+  }
+
+  echo 'Saving';
 }
 
 ?>
